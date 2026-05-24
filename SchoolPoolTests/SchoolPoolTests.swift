2d6a65321c797dd8adf246d37702ddc837a35d5d
Ronav Gupta <guptaronav8@gmail.com>
2026-05-24 00:39:05 -0700

    Initial Commit

---
+++ SchoolPoolTests/SchoolPoolTests.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolPoolTests.swift
+//  SchoolPoolTests
+//
+//  Created by Ronav Gupta on 5/24/26.
+//
+
+import Testing
+@testable import SchoolPool
+
+struct SchoolPoolTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
